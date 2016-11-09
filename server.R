7cf8c9ab4638557a4ea69c98db7862c2520a1862
vioxx <shomenuchi@gmail.com>
2016-11-09 13:34:52 -0500

    Updated version

--- server.R
+++ server.R
@@ -6,15 +6,22 @@ shinyServer(function(input, output) {
   })
   
   output$Plot <- renderPlot({
-    c <- data.frame(x=input$Y, y=input$X)
-    d <- data.frame(x=input$Z, y=input$Z)
-    ggplot(data=c,aes(x=x,y=y))+
-    geom_point() +
-    xlab('Observed Imprecision (%)') + ylab('Observed Inaccuracy (%)') +
-    geom_line(data=d, aes(x=c(x/2,0), y=c(0,y)), color = 'RED') +
-    geom_line(data=d, aes(x=c(x/3,0), y=c(0,y)), color = 'ORANGE') +
-    geom_line(data=d, aes(x=c(x/4,0), y=c(0,y)), color = 'YELLOW') +
-    geom_line(data=d, aes(x=c(x/5,0), y=c(0,y)), color = 'BLUE') +
-    geom_line(data=d, aes(x=c(x/6,0), y=c(0,y)), color = 'GREEN')
+    dt1 <- data.frame(Assay=c('Test'), CV=c(input$Y), Bias=c(input$X))
+    c <- data.frame(x=c(input$Z,0), y=c(0,input$Z))
+    ggplot(dt1, aes(x=CV, y=Bias)) + 
+      geom_point(size=4, aes(shape=Assay)) +
+      xlab('') + 
+      ylab('') +
+      ggtitle('') + 
+      geom_line(data=c, aes(x=c(input$Z/2,0), y=c(0,input$Z)), linetype=2, color = 'RED') + 
+      geom_line(data=c, aes(x=c(input$Z/3,0), y=c(0,input$Z)), linetype=2, color = 'ORANGE') +
+      geom_line(data=c, aes(x=c(input$Z/4,0), y=c(0,input$Z)), linetype=2, color = 'YELLOW') +
+      geom_line(data=c, aes(x=c(input$Z/5,0), y=c(0,input$Z)), linetype=2, color = 'BLUE') +
+      geom_line(data=c, aes(x=c(input$Z/6,0), y=c(0,input$Z)), linetype=2, color = 'GREEN') +
+#      annotate("text", x = 5, y = 13, label = "World Class") +
+#      annotate("text", x = 21.5, y = 30, label = "Marginal") +
+      ggtitle('Sigma-metric Performance') +
+      xlab('Observed Imprecision, % CV') +
+      ylab('Observed Inaccuracy, % Bias')
   })
 })
